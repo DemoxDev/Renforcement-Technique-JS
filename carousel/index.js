ea4142aa29fa096dc0d5f3b264807a0808cab249
DemoxDev <chrisdoesroblox@gmail.com>
2023-10-07 14:15:27 +0200

    Modifs + début caroussel

---
+++ carousel/index.js
@@ -0,0 +1,6 @@
+tab=["https://m.media-amazon.com/images/I/81vt8Zuy8AL._AC_UF1000,1000_QL80_.jpg", "https://www.cnet.com/a/img/resize/43bf7152f39f90a03df23c97a8a7ebb9a09ea520/hub/2022/02/23/f12a8db7-d99b-4b8d-9b09-d84f12661cf7/elden-ring-plakat.jpg?auto=webp&fit=bounds&height=1200&precrop=571,571,x357,y149&width=1200", "https://cdn.thewirecutter.com/wp-content/media/2022/03/elden-ring-2048px-0003.jpg?auto=webp&quality=75&width=1024"]
+
+export const moveSlide = (number) => {
+    return tab[number];
+}
+// quand on clique gauche tab[i-1], droite tab[i+1]
--- jeuxvideos-projet-a-rendre/views/jeuView.js
+++ jeuxvideos-projet-a-rendre/views/jeuView.js
@@ -1,24 +1,25 @@
 import { Jeu } from "../classes/jeu.js";
 
 export const displayJeu = () => {
-    let html = `<div class="relative w-[600px] mx-auto">`
+    let html = `<div class="relative w-[800px] mx-auto">` // Adjusted width
     for(let i of Jeu.listeJeux){
+        html += `<div class="slider-${i.idJeu}">`
         for(let j of i.photos) {
             html += `
             <div class="slide relative">
-                <img src="${j}" alt="" style="width: 500px; height: 250px;" class="transition-all ease-in-out duration-1000 transform translate-x-0" />
-            </div>`
+                <img src="${j}" alt="" style="width: 200px; height: 250px;" class="transition-all ease-in-out duration-1000 transform translate-x-0" />
+                <!-- Le bouton < -->
+                <a class="absolute-button p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
+                    onclick="moveSlide(-1)">❮</a>
+        
+                <!-- Le bouton > -->
+                <a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
+                    onclick="moveSlide(1)">❯</a>
+                </div>`
         }
         html += `
-        <!-- Le bouton < -->
-        <a class="absolute left-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(-1)">❮</a>
-
-        <!-- Le bouton > -->
-        <a class="absolute right-0 top-1/2 p-4 -translate-y-1/2 bg-black/30 hover:bg-black/50 text-white hover:text-amber-500 cursor-pointer"
-            onclick="moveSlide(1)">❯</a>
-        </div>
-        <br>
+        <!-- Placeholder -->
+        <div style="height: 250px;"></div>
         <div class="flex justify-center items-center space-x-5">
             <div class="dot w-4 h-4 rounded-full cursor-pointer" onclick="currentSlide(1)"></div>
             <div class="dot w-4 h-4 rounded-full cursor-pointer" onclick="currentSlide(2)"></div>
@@ -35,22 +36,19 @@ export const displayJeu = () => {
             <p class="genre">Genre: ${i.genre}</p>
             </div>
         </article>
+        </div>
         `
     }
     document.getElementById("showListeJeux").innerHTML=html;
-    console.log(html)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 1;
-    console.log("slideindex: ", slideIndex);
 
     function showSlide(n) {
         let i;
         const slides = document.getElementsByClassName("slide");
-        console.log("slides: ", slides);
         const dots = document.getElementsByClassName('dot');
-        console.log("dots: ", dots);
 
         if (n > slides.length) { slideIndex = 1 }
         if (n < 1) { slideIndex = slides.length }
@@ -60,14 +58,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         for (i = 0; i < dots.length; i++) {
-            dots[i].classList.remove('bg-yellow-500');
-            dots[i].classList.add('bg-green-600');
+            dots[i].classList.remove('bg-gray-600');
+            dots[i].classList.add('bg-gray-300');
         }
 
         slides[slideIndex - 1].classList.remove('hidden');
 
-        dots[slideIndex - 1].classList.remove('bg-green-600');
-        dots[slideIndex - 1].classList.add('bg-yellow-500');
+        dots[slideIndex - 1].classList.remove('bg-gray-300');
+        dots[slideIndex - 1].classList.add('bg-gray-600');
     }
 
     // Define the moveSlide function in the global scope
